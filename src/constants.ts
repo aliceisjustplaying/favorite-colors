301168559b896ed5d6880e15cb34325fc6b0de53
TKTK <TKTK>
2024-10-05 17:14:00 +0200

    Hello Causal Island

--- src/constants.ts
+++ src/constants.ts
@@ -1,46 +1,96 @@
 import { Label } from './types.js';
 
-export const DELETE = 'insert-rkey-of-delete-post-here';
+export const DELETE = '3l5rkt6pnq726';
 export const LABEL_LIMIT = 1;
 export const LABELS: Label[] = [
   {
-    rkey: 'insert-rkey-here',
-    identifier: 'earth',
+    rkey: '3l5rkt4j4wc2k',
+    identifier: 'red',
     locales: [
-      { lang: 'en', name: 'Earth 🌎', description: 'Earth'},
-      { lang: 'pt-BR', name: 'Terra 🌎', description: 'Terra'},
+      { lang: 'en', name: 'Red 🔴', description: 'Red'},
+      { lang: 'pt-br', name: 'Vermelho 🔴', description: 'Vermelho'},
+      { lang: 'ja', name: '赤 🔴', description: '赤'},
+      { lang: 'de', name: 'Rot 🔴', description: 'Rot'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
-    identifier: 'fire',
+    rkey: '3l5rkt4pqsf2j',
+    identifier: 'black',
     locales: [
-      { lang: 'en', name: 'Fire 🔥', description: 'Fire'},
-      { lang: 'pt-BR', name: 'Fogo 🔥', description: 'Fogo'},
+      { lang: 'en', name: 'Black ⚫️', description: 'Black'},
+      { lang: 'pt-br', name: 'Preto ⚫️', description: 'Preto'},
+      { lang: 'ja', name: '黒 ⚫️', description: '黒'},
+      { lang: 'de', name: 'Schwarz ⚫️', description: 'Schwarz'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
-    identifier: 'air',
+    rkey: '3l5rkt4wcvj2v',
+    identifier: 'blue',
     locales: [
-      { lang: 'en', name: 'Air 💨', description: 'Air'},
-      { lang: 'pt-BR', name: 'Ar 💨', description: 'Ar'},
+      { lang: 'en', name: 'Blue 🔵', description: 'Blue'},
+      { lang: 'pt-br', name: 'Azul 🔵', description: 'Azul'},
+      { lang: 'ja', name: '青 🔵', description: '青'},
+      { lang: 'de', name: 'Blau 🔵', description: 'Blau'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
-    identifier: 'water',
+    rkey: '3l5rkt54mxl2n',
+    identifier: 'white',
     locales: [
-      { lang: 'en', name: 'Water 💧', description: 'Water'},
-      { lang: 'pt-BR', name: 'Água 💧', description: 'Água'},
+      { lang: 'en', name: 'White ⚪️', description: 'White'},
+      { lang: 'pt-br', name: 'Branco ⚪️', description: 'Branco'},
+      { lang: 'ja', name: '白 ⚪️', description: '白'},
+      { lang: 'de', name: 'Weiß ⚪️', description: 'Weiß'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
-    identifier: 'love',
+    rkey: '3l5rkt5cu7u2i',
+    identifier: 'yellow',
     locales: [
-      { lang: 'en', name: 'Love 💞', description: 'Love'},
-      { lang: 'pt-BR', name: 'Amor 💞', description: 'Amor'},
+      { lang: 'en', name: 'Yellow 🟡', description: 'Yellow'},
+      { lang: 'pt-br', name: 'Amarelo 🟡', description: 'Amarelo'},
+      { lang: 'ja', name: '黄 🟡', description: '黄'},
+      { lang: 'de', name: 'Gelb 🟡', description: 'Gelb'},
+    ]
+  },
+  {
+    rkey: '3l5rkt5jb722s',
+    identifier: 'brown',
+    locales: [
+      { lang: 'en', name: 'Brown 🟤', description: 'Brown'},
+      { lang: 'pt-br', name: 'Marrom 🟤', description: 'Marrom'},
+      { lang: 'ja', name: '茶色 🟤', description: '茶色'},
+      { lang: 'de', name: 'Braun 🟤', description: 'Braun'},
+    ]
+  },
+  {
+    rkey: '3l5rkt5pobh2q',
+    identifier: 'green',
+    locales: [
+      { lang: 'en', name: 'Green 🟢', description: 'Green'},
+      { lang: 'pt-br', name: 'Verde 🟢', description: 'Verde'},
+      { lang: 'ja', name: '緑 🟢', description: '緑'},
+      { lang: 'de', name: 'Grün 🟢', description: 'Grün'},
+    ]
+  },
+  {
+    rkey: '3l5rkt6bfo52j',
+    identifier: 'orange',
+    locales: [
+      { lang: 'en', name: 'Orange 🟠', description: 'Orange'},
+      { lang: 'pt-br', name: 'Laranja 🟠', description: 'Laranja'},
+      { lang: 'ja', name: 'オレンジ 🟠', description: 'オレンジ'},
+      { lang: 'de', name: 'Orange 🟠', description: 'Orange'},
+    ]
+  },
+  {
+    rkey: '3l5rkt6hznf2u',
+    identifier: 'purple',
+    locales: [
+      { lang: 'en', name: 'Purple 🟣', description: 'Purple'},
+      { lang: 'pt-br', name: 'Roxo 🟣', description: 'Roxo'},
+      { lang: 'ja', name: '紫 🟣', description: '紫'},
+      { lang: 'de', name: 'Lila 🟣', description: 'Lila'},
     ]
   },
 ];
